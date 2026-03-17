0aadbe5fc7e996322522e522a1cbc4da2ca2c10f
Poorvi <poorvigb@gmail.com>
2026-03-17 20:50:24 +0530

    Update Supabase Anon Key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://gqkzivcrswqphhuxjrab.supabase.co";
-const supabaseAnonKey = "sb_publishable_OIrUD1EIDtEzjOjtb6H4kg_djZpxQ-q";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdxa3ppdmNyc3dxcGhodXhqcmFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MDgwMDQsImV4cCI6MjA4ODM4NDAwNH0.KxsL8bT9GuoB0-KQwVpT0B8mp7SGuG9Gyd79LP8NN4A";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
